2f647dca9e0334f52a8c8108e50d815abf25bed8
iamalan07 <89475164+iamalan07@users.noreply.github.com>
2022-05-24 22:44:31 +0530

    Add files via upload

---
+++ securitygroup.tf
@@ -0,0 +1,62 @@
+## cat bastionhost-sg.tf
+resource "aws_security_group" "ingress-all-test" {
+name = "bastion-host-sg"
+vpc_id = module.vpc.vpc_id
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+// Terraform removes the default rule
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+### cat privateweb-sg.tf
+resource "aws_security_group" "ingress-all-sg" {
+name = "public-web-sg"
+vpc_id = module.vpc.vpc_id
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+// Terraform removes the default rule
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+### cat private-sg.tf
+resource "aws_security_group" "ingress-all" {
+name = "Private-Instances-sg"
+vpc_id = module.vpc.vpc_id
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+// Terraform removes the default rule
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
